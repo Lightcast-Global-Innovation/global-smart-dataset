648f1adfe707ce262f0eb1fcc3c899c682697c90
Mauro Pelucchi <mauro.pelucchi@gmail.com>
2022-07-30 18:40:15 +0200

    fix bugs

--- lightcast_smart_dataset/setup.py
+++ lightcast_smart_dataset/setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='lightcast-smart-dataset',
-    version='0.1.25',
+    version='0.1.26',
     scripts=['lightcast-smart-dataset'],
     author="Global Data Science Team - Lightcast",
     author_email="globaldatascience@lightcast.io",
